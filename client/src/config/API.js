b58ecc3c692c9d9e04e48961806ff8203f4ba947
Krishnay mudgal <kmud2208@gmail.com>
2022-11-21 18:26:30 +0530

    Added API

--- client/src/config/API.js
+++ client/src/config/API.js
@@ -1,5 +1,6 @@
 // export const API_SERVICE = "https://guestclub.herokuapp.com/api/v1/main";
-export const API_SERVICE = "https://task-manager-dpl.herokuapp.com/api/v1/main";
+//export const API_SERVICE = "https://task-manager-dpl.herokuapp.com/api/v1/main";
+export const API_SERVICE = "https://taskmanagementsaas.herokuapp.com/";
 export const BASE_URL = "https://task-management-saas.netlify.app/";
 
 // export const API_SERVICE = "http://localhost:5000/api/v1/main";
