8085f8584c03bba79f4fce98152c116527feab0c
Krishnay mudgal <kmud2208@gmail.com>
2022-11-21 17:16:54 +0530

    change config

--- client/src/config.js
+++ client/src/config.js
@@ -26,8 +26,8 @@ export const gtmConfig = {
   containerId: process.env.NEXT_PUBLIC_GTM_CONTAINER_ID,
 };
 
-// export const API_SERVICE = "https://task-manager-dpl.herokuapp.com/api/v1/main";
-export const API_SERVICE = "http://localhost:5000/api/v1/main";
+ export const API_SERVICE = "https://task-manager-dpl.herokuapp.com/api/v1/main";
+//export const API_SERVICE = "http://localhost:5000/api/v1/main";
 // export const API_SERVICE = "https://task-management-sass.herokuapp.com/api/v1/main";
-// export const API_SERVICE = "https://taskmanagementapidev.herokuapp.com/api/v1/main";
+ //export const API_SERVICE = "https://taskmanagementapidev.herokuapp.com/api/v1/main";
 
